2d1779e3123eb0ec65ce958f58ff297a865ef12c
Mujib <muhdiqmal1997@gmail.com>
2022-09-03 22:13:27 +0800

    update page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,28 +1,36 @@
+<script setup>
+  import HomePage from "@/components/HomePage.vue";
+  import ExperiencePage from "@/components/ExperiencePage.vue";
+</script>
+
 <template>
-  <div class="aboutContainer">
-    <HomePage class="homePage"/>
-    <div class="about">
-      <div class="title">
-        <h1>About me</h1>
-      </div>
-      <div class="aboutintro">
-        <p>I am a Python developer with experience in API development using Flask and Django framework, building automated solution to support both strategic and tactical plans, which includes operations and engineering of my current company. I have experience in full-stack web development using Django, PorstgresSQL, Docker and Google Cloud Kubernetes Engine. Currently, I am looking into front-end web development such as Vuejs, SCSS and Nodejs.</p>
-      </div>
+  <div class="aboutpage">
+    <div class="aboutContainer">
+      <HomePage class="homePage"/>
+      <div class="about">
+        <div class="title">
+          <h1>About me</h1>
+        </div>
+        <div class="aboutintro">
+          <p>I am a Python developer with experience in API development using Flask and Django framework, building automated solution to support both strategic and tactical plans, which includes operations and engineering of my current company. I have experience in full-stack web development using Django, PorstgresSQL, Docker and Google Cloud Kubernetes Engine. Currently, I am looking into front-end web development such as Vuejs, SCSS and Nodejs.</p>
+        </div>
+    
+    </div>
+    </div>
+    
+    <ExperiencePage/>
     
   </div>
-  </div>
+
 
 </template>
-
-<script setup>
-import HomePage from "@/components/HomePage.vue";
-</script>
 
 <script>
 export default {
   name: "About",
   components: {
-    HomePage
+    HomePage,
+    ExperiencePage
   },
 }
 </script>
@@ -36,20 +44,21 @@ export default {
   0%{left:15rem;}
   
 }
+.aboutpage{
+  top:2.5rem;
+  min-height:200vh;
+  display: block;
+}
 .aboutContainer{
-  display:flex;
+  display:block;
 }
 .homePage{
   left:-50rem;
-  z-index:2;
   animation: slide-profile 1s ease-in-out;
   position: absolute;
 }
 
 .title{
-  text-align: center;
-  top:3rem;
-  left:2.5rem;
   font-family: 'Oooh Baby', cursive;
   color: #428fe6;
   width: 100vh;
@@ -58,17 +67,15 @@ export default {
 }
 
 .about {
-  left:40rem;  
-  width: 125vh;
+  left:50rem;  
+  width: 80vh;
   height: 25vh;
   animation: slide-background 1s ease-in-out;
   position: absolute;
-  z-index: 1;
+
 }
 
 .aboutintro{
-  top:8rem;
-  left:-8rem;
   font-family: 'Roboto',sans-serif;
   font-size:2rem;
   font-weight: 10;
@@ -77,10 +84,9 @@ export default {
 
 
 
-@media (min-width: 1024px) {
+/* @media (min-width: 1024px) {
   .about {
-    min-height: 70vh;
-    display: flex;
+    min-height: 100vh;
   }
-}
+} */
 </style>
